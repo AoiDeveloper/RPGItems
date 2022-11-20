b2799b0f5e60b80a9903b48afa46a82d2eebe334
AoiDeveloper <76095792+AoiDeveloper@users.noreply.github.com>
2022-11-20 22:24:20 +0900

    add tab completer

--- src/main/kotlin/com/aoideveloper/rpg_items/RPGItems.kt
+++ src/main/kotlin/com/aoideveloper/rpg_items/RPGItems.kt
@@ -1,6 +1,7 @@
 package com.aoideveloper.rpg_items
 
 import com.aoideveloper.rpg_items.command.RPGItemCommand
+import com.aoideveloper.rpg_items.command.RPGItemCommandTabCompleter
 import com.aoideveloper.rpg_items.event.ClickEvent
 import com.aoideveloper.rpg_items.event.CraftEvent
 import com.aoideveloper.rpg_items.item.ItemManager
@@ -17,6 +18,7 @@ class RPGItems: JavaPlugin() {
         plugin = this
 
         getCommand("rpgitems")?.setExecutor(RPGItemCommand())
+        getCommand("rpgitems")?.tabCompleter = RPGItemCommandTabCompleter()
         server.pluginManager.registerEvents(ClickEvent(), this)
         server.pluginManager.registerEvents(CraftEvent(), this)
         dataFolder.mkdir()
